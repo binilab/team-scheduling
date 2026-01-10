18abd435a0e030bdb3d72d10b7f18a0b0ac73ebc
wonbin <rladnjsqls94@gmail.com>
2026-01-11 04:24:58 +0900

    mvp10

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,4 @@
-const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000"
+const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://ourstime.com"
 
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
